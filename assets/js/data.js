e90c7ec7cbeb8563ac0d427d4842ebd146a80848
apodicticscott <apodicticscott@protonmail.com>
2022-04-24 17:10:13 -0400

    DondaMusic WebApp v1
    Created a HTML, CSS, and JS Donda music player. Will later add different albums, and some more controls to the site.

---
+++ assets/js/data.js
@@ -0,0 +1,92 @@
+let songs = [
+    {
+        name: 'TRUE LOVE',
+        path: 'assets/music/01 TRUE LOVE (feat. XXXTENTACION).mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'BROKEN ROAD',
+        path: 'assets/music/02 BROKEN ROAD (feat. Don Toliver).mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'GET LOST',
+        path: 'assets/music/03 GET LOST.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'TOO EASY',
+        path: 'assets/music/04 TOO EASY.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'FLOWERS',
+        path: 'assets/music/05 FLOWERS.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'SECURITY',
+        path: 'assets/music/06 SECURITY.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'WE DID IT KID',
+        path: 'assets/music/07 WE DID IT KID.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'PABLO',
+        path: 'assets/music/08 PABLO.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'LOUIE BAGS',
+        path: 'assets/music/09 LOUIE BAGS.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'HAPPY',
+        path: 'assets/music/10 HAPPY.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'SCI FI',
+        path: 'assets/music/11 SCI FI.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'SELFISH',
+        path: 'assets/music/12 SELFISH.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'LORD LIFT ME UP',
+        path: 'assets/music/13 LORD LIFT ME UP.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'KEEP IT BURNING',
+        path: 'assets/music/14 KEEP IT BURNING.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+    {
+        name: 'FIRST TIME',
+        path: 'assets/music/15 FIRST TIME IN A LONG TIME.mp3',
+        artist: 'Kanye West',
+        cover: 'assets/img/cover.jpg'
+    },
+]
